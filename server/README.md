68ec523863c827cd6d4c12c4a91cbed8e9569bac
Vladimir <vladimireur@gmail.com>
2022-06-16 18:08:09 +0300

    Initialisation project

---
+++ server/README.md
@@ -0,0 +1,15 @@
+# GraphQl-server-MongoDB
+
+Before start:
+
+create mongoDb account and database
+
+npm install
+
+npm i nodemon -g
+
+get URL mongoDb database and add to index.js
+
+To start:
+
+nodemon index.js
---
+++ server/resolvers.js
@@ -0,0 +1,48 @@
+import Todo from './models/Todo.js'
+
+const resolvers = {
+    Query: {
+        welcome: () => {
+            return 'Welcome to ckmobile'
+        },
+        getTodos: async() => {
+            const todos = await Todo.find()
+            return todos
+        },
+
+        getTodo: async( root, args ) => {
+            const todo = await Todo.findById( args.id )
+            return todo
+        },
+    },
+
+    Mutation: {
+        addTodo:
+            async( root, args ) => {
+                const newTodo = new Todo( {
+                    title: args.title,
+                    detail: args.detail,
+                    date: args.date,
+                } )
+                await newTodo.save()
+                return newTodo
+            },
+        deleteTodo:
+            async( root, args ) => {
+                await Todo.findByIdAndDelete( args.id )
+                return 'The todo deleted successfully!!!'
+            },
+        updateTodo:
+            async( root, args ) => {
+                const { id, title, detail, date } = args
+                const updatedTodo = {}
+                if( title ) updatedTodo.title = title
+                if( detail ) updatedTodo.detail = detail
+                if( date ) updatedTodo.date = date
+                const todo = await Todo.findByIdAndUpdate( id, updatedTodo, { new: true } )
+                return todo
+            },
+    },
+}
+
+export default resolvers
